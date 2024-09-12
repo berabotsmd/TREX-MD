cafdad2bcbb0d8c2ad427c6c796e79b3577b7943
berabotsmd <berahiggins@gmail.com>
2024-09-13 00:28:21 +0300

    Trex

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -4,10 +4,10 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *𝑩𝑼𝑮𝑨𝑻𝑻𝑰* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *𝑴𝒂𝒓𝒊𝒔𝒆𝒍 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝒊𝒕☆*';
+    let z = 'Salut je m\'appelle *𝐓𝐑𝐄𝐗 𝐌𝐃* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *𝐁𝐄𝐑𝐀 𝐓𝐄𝐂𝐇 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝒊𝒕☆*';
     let varmess = z + d;
-    var lien = 'https://whatsapp.com/channel/0029Vajvy2kEwEjwAKP4SI0x';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    var lien = 'https://whatsapp.com/channel/0029VajJoCoLI8YePbpsnE3q';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
 });
 
