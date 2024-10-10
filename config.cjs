48cf72a10b5316b011f612bdfddc335cee04eaca
marisel <140407389+betingrich@users.noreply.github.com>
2024-10-10 19:38:05 +0300

    TREX MD

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&jcuK9hqs",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&Fs0ffEcr",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
