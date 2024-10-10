df55a8dfc68eda673d55968658b9f1cf48240345
marisel <140407389+betingrich@users.noreply.github.com>
2024-10-10 09:07:17 +0300

    TREX MD

--- config.cjs
+++ config.cjs
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "Marisel",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254740007567",
+  OWNER_NAME: process.env.OWNER_NAME || "Bera",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254743982206",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
